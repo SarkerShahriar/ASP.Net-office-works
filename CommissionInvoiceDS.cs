847f8a7d8239cfc6f8fcc5ae9d30d9730f9731e3
Sarker Shahriar Gajnabi <31381799+SarkerShahriar@users.noreply.github.com>
2022-01-01 15:33:29 +0600

    Crystal Report Dataset
    Dataset system

---
+++ CommissionInvoiceDS.cs
@@ -0,0 +1,21 @@
+﻿namespace BHMS.ReportDataset
+    {
+    }
+
+namespace BHMS.ReportDataset
+    {
+    }
+namespace BHMS.ReportDataset
+    {
+
+
+    public partial class CommissionInvoiceDS
+        {
+        }
+    }
+namespace BHMS.ReportDataset {
+    
+    
+    public partial class CommissionInvoiceDS {
+    }
+}
